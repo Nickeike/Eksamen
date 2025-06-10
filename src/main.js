ac06131eb81802ae6805e1ede6ff63e72840728f
Nickeike <nickeike@outlook.com>
2025-06-10 11:01:35 +0200

    modified:   html/index.html 	modified:   html/personvern.html 	modified:   src/main.js 	modified:   stiler/styles.css

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Funksjon for å handtere påmelding til lanseringsfest
+document.getElementById('meld-paa-knapp').addEventListener('click', () => {
+    // Viser ei melding når brukaren melder seg på
+    alert('Takk for påmeldinga! Me ser fram til å sjå deg på lanseringsfesten.');
+});
+
+// Funksjon for å legge til dynamisk innhald (kan utvidast)
+window.onload = () => {
+    console.log('Nettsida er lasta inn.');
+};
